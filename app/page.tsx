c12aa2cbb1ec2b1d3bea0bd84bffd367391b8e4a
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-06-18 17:55:16 +0300

    Fix HomePage Navigator

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { apiCallRevalidate } from "@/api/data";
 import PageNavigator from "@/layout/PageNavigator";
-import HappyClientsSection from "@/sections/clients";
-import TestimonialsSection from "@/sections/testimonials";
 import {
   faBriefcase,
   faCode,
+  faFaceSmile,
+  faHome,
   faPerson,
   faPersonCircleCheck,
+  faStar,
 } from "@fortawesome/free-solid-svg-icons";
 import dynamic from "next/dynamic";
 import { Fragment } from "react";
@@ -19,12 +20,15 @@ export default async function Home() {
   const CareerSection = dynamic(() => import("@/sections/experiences"));
   const WorksSection = dynamic(() => import("@/sections/works/works"));
   const SkillsSection = dynamic(() => import("@/sections/skills"));
+  const HappyClientsSection = dynamic(() => import("@/sections/clients"));
+  const TestimonialsSection = dynamic(() => import("@/sections/testimonials"));
 
   const sections = [
-    { id: "about", label: "About", icon: faPerson },
-    { id: "experiences", label: "Experiences", icon: faBriefcase },
+    { id: "home", label: "Home", icon: faHome },
     { id: "works", label: "Works", icon: faCode },
     { id: "skills", label: "Skills", icon: faPersonCircleCheck },
+    { id: "clients", label: "Clients", icon: faFaceSmile },
+    { id: "testimonials", label: "Testimonials", icon: faStar },
   ];
 
   return (
