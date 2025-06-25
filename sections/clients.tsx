1e0a860bff3ad2a4325e4a02a1cfc2e414cdc33d
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-06-25 21:15:06 +0300

    Add Filter To Clients

--- sections/clients.tsx
+++ sections/clients.tsx
@@ -15,16 +15,25 @@ const ProminentClientsSection = async () => {
   return (
     <PageSection title="Prominent Clients" color="light" NoBg id="clients">
       <Row className="justify-content-center">
-        {clients.map(({ img, link }, i) => (
-          <Col xs={6} sm={4} md={3} lg={2} className="text-center p-5" key={i}>
-            <a href={link} target="_blank" className="text-decoration-none">
-              <img
-                src={`https://firebasestorage.googleapis.com/v0/b/resume-data-8215f.appspot.com/o/${img}?alt=media`}
-                className="w-100"
-              />
-            </a>
-          </Col>
-        ))}
+        {clients
+          .filter(({ prominent }) => prominent)
+          .map(({ img, link }, i) => (
+            <Col
+              xs={6}
+              sm={4}
+              md={3}
+              lg={2}
+              className="text-center p-5"
+              key={i}
+            >
+              <a href={link} target="_blank" className="text-decoration-none">
+                <img
+                  src={`https://firebasestorage.googleapis.com/v0/b/resume-data-8215f.appspot.com/o/${img}?alt=media`}
+                  className="w-100"
+                />
+              </a>
+            </Col>
+          ))}
       </Row>
     </PageSection>
   );
