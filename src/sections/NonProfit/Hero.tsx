a721bc83e7b9e7d7b20bc3b6d9e7266fbd86ead9
Makkahwi <SuhaibAhmadAi@hotmail.com>
2026-01-08 09:35:39 +0300

    build blog page contents

--- src/sections/NonProfit/Hero.tsx
+++ src/sections/NonProfit/Hero.tsx
@@ -2,11 +2,7 @@ import PageSection from "@/components/PageSection";
 
 const NonProfitHeroSection = async ({ t }: { t: Function }) => {
   return (
-    <PageSection
-      title={t("NonProfit.Title")}
-      subtitle={t("NonProfit.Subtitle")}
-      id="hero"
-    >
+    <PageSection title={t("Title")} subtitle={t("Subtitle")} id="hero">
       {
         "Life isn't only about making money, or doing something and expect materialistic returns, one could do something out of payback and favour return to community."
       }
