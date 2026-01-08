25f7013d01050b13463e9edbcbaad1237192a18a
Makkahwi <SuhaibAhmadAi@hotmail.com>
2026-01-08 09:38:08 +0300

    build missing profile image @ home page

--- src/sections/Home/Hero.tsx
+++ src/sections/Home/Hero.tsx
@@ -1,10 +1,19 @@
 import { statisticsList } from "@/api/hardCodedData";
 import StatisticCard from "@/components/Pages/StatisticCard";
+import Image from "next/image";
 
 const HomeHeroSection = async ({ t }: { t: Function }) => {
   return (
     <div>
       <header className="mb-5">
+        <Image
+          src={
+            process.env.NEXT_PUBLIC_STORAGE_URL + "profile.jpg" + "?alt=media"
+          }
+          alt="Logo"
+          width={150}
+          height={150}
+        />
         <h1>{t("Title")}</h1>
         <p className="lead">{t("Subtitle")}</p>
       </header>
