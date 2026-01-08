06a2d8db4ffb0a108001c60b44290e7a91fde4e1
Makkahwi <SuhaibAhmadAi@hotmail.com>
2026-01-08 10:20:01 +0300

    fix db data structure & its integration

--- src/api/hardCodedData.ts
+++ src/api/hardCodedData.ts
@@ -13,16 +13,16 @@ import {
 
 import json from "./data.json";
 
-export const clientsList: ClientProps[] = json.clients;
-export const blogPosts: BlogProps[] = json.blog;
-export const jobsList: JobProps[] = json.jobs[0].jobs;
-export const traineesList: TraineeProps[] = json.trainees;
-export const projectsList: ProjectProps[] = json.jobProjects;
-export const skillsList: SkillsProps[] = json.skills;
-export const testimonialsList: TestimonialProps[] = json.testimonials;
+export const clientsList: ClientProps[] = json.developer.clients;
+export const blogPosts: BlogProps[] = json.developer.blog;
+export const jobsList: JobProps[] = json.developer.jobs;
+export const traineesList: TraineeProps[] = json.developer.trainees;
+export const projectsList: ProjectProps[] = json.developer.projects;
+export const skillsList: SkillsProps[] = json.developer.skills;
+export const testimonialsList: TestimonialProps[] = json.developer.testimonials;
 
-export const contactsList: ContactProps[] = json.contacts;
-export const educationsList: EducationProps[] = json.education;
+export const contactsList: ContactProps[] = json.common.contacts;
+export const educationsList: EducationProps[] = json.common.education;
 
 export const statisticsList: StatisticProps[] = [
   {
