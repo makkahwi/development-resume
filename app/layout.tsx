a2f8562107315f75070d1a377fbe50ae8eade82d
Makkahwi <SuhaibAhmadAi@hotmail.com>
2026-02-03 14:47:14 +0300

    add microsoft clarity integration code

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,16 @@ export default function RootLayout({
             gtag('config', 'G-WB2XN091EX');
           `}
         </Script>
+
+        <Script id="clarity-tag" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vbkug8drim");
+          `}
+        </Script>
       </body>
     </html>
   );
