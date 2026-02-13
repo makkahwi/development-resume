d60ecffa9263ccc4850f785645606a2728b67a68
makkahwi <suhaibahmadai@hotmail.com>
2026-02-13 16:20:04 +0300

    feat(common): pre-commit husky

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 To do list
 
-- centralize components (button)
-- apply brand colors to theme
-- on route change scroll to top
-- build contact section @ layout
 - turn page navigator into scroll-based navigator
-- build husky pre-commit hooks
 - build auto-versioning
 - build documentation
 - build admin panel
---
+++ commitlint.config.mjs
@@ -0,0 +1,25 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // A new feature
+        "fix", // A bug fix
+        "docs", // Documentation only changes
+        "style", // Changes that do not affect the meaning of the code (white-space, formatting, missing semicolons, etc)
+        "refactor", // A code change that neither fixes a bug nor adds a feature
+        "perf", // A code change that improves performance
+        "test", // Adding missing tests or correcting existing tests
+        "chore", // Changes that don't modify src or test files
+        "ci", // Changes to CI configuration files and scripts
+      ],
+    ],
+    "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+  },
+};
